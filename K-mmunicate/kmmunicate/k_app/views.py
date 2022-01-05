7f1250d061496bb01d30e6b1f6f0017a65af54b8
ernieenero1121 <ernieenero1121@gmail.com>
2022-01-05 17:45:56 +0800

    added memoization to result view, please check for any bugs

--- K-mmunicate/kmmunicate/k_app/views.py
+++ K-mmunicate/kmmunicate/k_app/views.py
@@ -10,7 +10,6 @@ import json
 import os
 
 
-
 # ===================== VARIABLES  & CLASSES ==========================
 
 with open(os.path.join(settings.STATIC_DIR, 'k_app\model_FSL.json'), 'r') as file1:
@@ -20,60 +19,52 @@ with open(os.path.join(settings.STATIC_DIR, 'k_app\model_FSL.json'), 'r') as fil
 fsl_model.load_weights(os.path.join(settings.STATIC_DIR, 'k_app\model_FSL.h5'))
 
 
-
 class detection(object):
 
     def __init__(self):
         # self.dataset = fsl_dataset   #DICTIONARY - fsl_dataset['a']["10"][10]
         self.model = fsl_model
         self.hand_response = []
-        self.action_labels = np.array(['a','b','c'])
+        self.action_labels = np.array(['a', 'b', 'c'])
         self.res = []
         self.result = 'hold'
         self.threshold = 0.999
 
-
     def fsl_predict(self):
-        self.res = self.model.predict(np.expand_dims(self.hand_response, axis=0))[0]
+        self.res = self.model.predict(
+            np.expand_dims(self.hand_response, axis=0))[0]
 
-
-        if self.res[np.argmax(self.res)] > self.threshold: 
+        if self.res[np.argmax(self.res)] > self.threshold:
             self.result = str(self.action_labels[np.argmax(self.res)])
-            
+
         else:
             self.result = "Please try again"
-      
 
     def get_predictions(self, hand_response):
 
         self.hand_response = hand_response
         check = np.array(self.hand_response)
-       
-        #NO HANDS CHECKER
+
+        # NO HANDS CHECKER
         if np.argmax(check) == 0:
             # print('NO HANDS!')
-            self.result =  "No hands detected"
+            self.result = "No hands detected"
         else:
 
             self.fsl_predict()
-            
-
-
 
 
 # =========================== MY VIEWS ===============================
 
 def index(request):
-    return render(request,'k_app/landing.html')
-
+    return render(request, 'k_app/landing.html')
 
 
 # @csrf_protect
-@csrf_exempt 
+@csrf_exempt
 def kcam(request):
 
     return render(request, 'k_app/cam_page.html')
-
 
 
 def result(request):
@@ -82,7 +73,21 @@ def result(request):
         # print(request.POST['keypoints'])
         hand_response = request.POST['keypoints'].split(',')
         # print("LENGTH : ",hand_response)  #THE COMMA'S ARE INCUDED AS STRING, FIX THIS SOON!
-        raw_keypoints = [float(num) for num in list(hand_response) if num != ',']
+
+        # raw_keypoints = [float(num)
+        #                  for num in list(hand_response) if num != ',']
+        raw_keypoints = []
+
+        def recurrsion(listing):
+
+            raw_keypoints.append(float(listing[0]))
+
+            if len(listing) == 1:
+                return listing
+            else:
+                return recurrsion(listing[1:])
+        recurrsion(hand_response)
+
         # print(raw_keypoints, len(raw_keypoints))
         p = detection()
         p.get_predictions(raw_keypoints)
@@ -90,17 +95,16 @@ def result(request):
         # return render(request, 'k_app/kcam.html', {'signs': p.result})
         return HttpResponse(json.dumps({'signs': p.result}))
 
-        #FUNCTION : This also reload the page, so find a way stay on the page.
+        # FUNCTION : This also reload the page, so find a way stay on the page.
 
     # return render(request, 'k_app/kcam.html', {'signs': p.result}) #, {'form': x} #,
-
 
 
 # # else:
 #     #     pass
 #         # return()
 
-#         #THROWS ValueError: Input 0 of layer "sequential" is incompatible 
+#         #THROWS ValueError: Input 0 of layer "sequential" is incompatible
 #         # with the layer: expected shape=(None, 126), found shape=(None, 1369)
 
 #         #FUNCTION : This also reload the page, so find a way stay on the page.
@@ -108,16 +112,11 @@ def result(request):
 #     return render(request, 'k_app/kcam.html', {'signs': p.result}) #, {'form': x} #,
 
 
-
-
-
 #from django.views.decorators.csrf import csrf_protect
 # from django.core import serializers
 #import mediapipe as mp
 # from gtts import gTTS
 # from . import forms
-
-
 
   # def fsl_to_speech(self):
     #     language = 'en'
@@ -138,22 +137,18 @@ def result(request):
 
         # return context
 
-        #play mp3 file
+        # play mp3 file
         # os.system("start os.path.join(settings.STATIC_DIR, 'k_app/fsl_audio.mp3')")
         # os.system('fsl_audio.mp3')
-
-
-
-
 
   # keyForm = forms.KeypointForm()
   # print(type(request.body))
 
-#=============TEXT TO SPEECH =======================
+# =============TEXT TO SPEECH =======================
     # p.fsl_to_speech()
 
     # print(type(post_result))
-        # H_SIGN = 
+        # H_SIGN =
         # keyForm['sign'].value = H_SIGN
 
     # print(type(sign_json))
@@ -171,13 +166,13 @@ def result(request):
         # render(HttpResponse(sign))
         # return HttpResponse(sign['signs'],  content_type="text/plain")
 
-         # t = Template("SIGN : {{ sign }}.")
+        # t = Template("SIGN : {{ sign }}.")
         # c = Context({"sign": sign})
         # render(HttpResponse(Context({'signs':sign})))
         # template = loader.get_template('k_app/kcam.html')
         # return HttpResponse(template.render(sign, request))
         # print(sign['signs'])
-        
+
         # sign_res = serializers.serialize("json", sign)
         # return JsonResponse(sign_res, content_type='application/json', safe=False)
         # html = '<div>Hello World</div>'
@@ -186,6 +181,3 @@ def result(request):
         # html = "<h4>{sign.s}</h4>"
         # return HttpResponse(html, content_type='text/html')
     # print('content : ', sign_json, 'type : ', type(sign_json))
-
-
-
